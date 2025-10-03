7efed50a2566bc51efd284c5ebcb37ac9cacdecc
Johnny Heggelund <morepcfun@gmail.com>
2025-10-03 17:52:17 +0200

    Delete hello.js

--- hello.js
+++
@@ -1,9 +0,0 @@
-programs['hello'] = {
-    description: 'A simple hello world program.',
-    execute: async (term, args) => {
-        term.print("Hello from an external file!");
-        if (args.length > 0) {
-            term.print(`You passed in these arguments: ${args.join(', ')}`);
-        }
-    }
-};
