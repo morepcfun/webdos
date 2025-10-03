8c15eaea6a16eecf56e9df36d77b45320fc987c3
Johnny Heggelund <morepcfun@gmail.com>
2025-10-03 17:52:35 +0200

    Delete about.js

--- about.js
+++
@@ -1,7 +0,0 @@
-programs['about'] = {
-    description: 'Prints information about this system.',
-    execute: async (term, args) => {
-        term.print("WEB-DOS [Version 1.0]");
-        term.print("(c) 2025. A self-contained terminal environment.");
-    }
-};
